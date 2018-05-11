6e8d1952157f3e817643becd55ff64351e36b125
Vincent-Philippe Lauzon <vilauzon@microsoft.com>
2018-05-11 13:10:23 -0400

    Hide / show section

--- CognitiveHackSol/PhotoBrowser/wwwroot/scripts/logic/status.js
+++ CognitiveHackSol/PhotoBrowser/wwwroot/scripts/logic/status.js
@@ -1,10 +1,13 @@
-﻿function status_refreshStatus(sessionId, status, imageCount) {
+﻿function status_refreshStatus(sessionId, status, imageCount, onReady) {
     statusApiProxy_requestStatus(
         sessionId,
         function (statusValue, imageCountValue) {
             status.textContent = statusValue;
             imageCount.textContent = imageCountValue;
-            if (statusValue != "ready") {
+            if (statusValue == "ready") {
+                onReady();
+            }
+            else {
                 setTimeout(
                     function () {
                         status_refreshStatus(sessionId, status, imageCount);
--- CognitiveHackSol/PhotoBrowser/wwwroot/scripts/pages/home.js
+++ CognitiveHackSol/PhotoBrowser/wwwroot/scripts/pages/home.js
@@ -2,6 +2,13 @@
     var sessionId = session_getSessionId();
     var status = document.getElementById('status');
     var imageCount = document.getElementById('imageCount');
+    var photoSection = document.getElementById('photoSection');
 
-    status_refreshStatus(sessionId, status, imageCount);
+    status_refreshStatus(
+        sessionId,
+        status,
+        imageCount,
+        function () {
+            photoSection.style.display = 'block';
+        });
 }
