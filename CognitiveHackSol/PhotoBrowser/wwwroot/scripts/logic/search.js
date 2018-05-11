6dde5bb641991a09727496233ddfbde3c5ed11fe
Vincent-Philippe Lauzon <vilauzon@microsoft.com>
2018-05-11 15:00:58 -0400

    Show images

--- CognitiveHackSol/PhotoBrowser/wwwroot/scripts/logic/search.js
+++ CognitiveHackSol/PhotoBrowser/wwwroot/scripts/logic/search.js
@@ -1,9 +1,34 @@
-﻿function search_searchNoCriteria(sessionId, photoText) {
+﻿function search_searchNoCriteria(sessionId, imageResult, imageResultTemplate) {
     searchApiProxy_searchNoCriteria(
         sessionId,
-        function (payload) {
-            photoText.value = JSON.stringify(payload);
+        function (result) {
+            search_displayResults(result, imageResult, imageResultTemplate);
         },
         function () {
         })
+}
+
+function search_displayResults(result, imageResult, imageResultTemplate) {
+    //  Clear results
+    imageResult.innerHTML = "";
+
+    for (var i = 0; i < result.length; ++i) {
+        var clone = imageResultTemplate.cloneNode(true);
+
+        search_appendIds(clone, i);
+        imageResult.appendChild(clone);
+
+        var thumbnail = document.getElementById('thumbnail' + i);
+
+        thumbnail.src = result[i].thumbnailUrl;
+        clone.style.display = 'block';
+    }
+}
+
+function search_appendIds(element, index) {
+    element.id += index;
+
+    for (var i = 0; i < element.children.length; ++i) {
+        search_appendIds(element.children[i], index);
+    }
 }
--- CognitiveHackSol/PhotoBrowser/wwwroot/scripts/pages/home.js
+++ CognitiveHackSol/PhotoBrowser/wwwroot/scripts/pages/home.js
@@ -2,15 +2,16 @@
     var sessionId = session_getSessionId();
     var status = document.getElementById('status');
     var imageCount = document.getElementById('imageCount');
-    var photoSection = document.getElementById('photoSection');
-    var photoText = document.getElementById('photos');
+    var imageSection = document.getElementById('imageSection');
+    var imageResult = document.getElementById('imageResult');
+    var imageResultTemplate = document.getElementById('imageResultTemplate');
 
     status_refreshStatus(
         sessionId,
         status,
         imageCount,
         function () {
-            photoSection.style.display = 'block';
-            search_searchNoCriteria(sessionId, photoText);
+            imageSection.style.display = 'block';
+            search_searchNoCriteria(sessionId, imageResult, imageResultTemplate);
         });
 }
