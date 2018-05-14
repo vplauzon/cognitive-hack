43f47018d899569a46273dfade541f9fd71d9b7d
Vincent-Philippe Lauzon <vilauzon@microsoft.com>
2018-05-14 12:27:40 -0400

    Scafold change criteria

--- CognitiveHackSol/PhotoBrowser/wwwroot/scripts/logic/searchCriteria.js
+++ CognitiveHackSol/PhotoBrowser/wwwroot/scripts/logic/searchCriteria.js
@@ -1,1 +1,35 @@
-﻿
+﻿function searchCriteria_bindCriteria(tagCount) {
+    var criteria = { tags: [] };
+
+    for (var i = 0; i != tagCount; ++i) {
+        var inputBox = document.getElementById("tagCheckBox" + i);
+
+        searchCriteria_onClick(criteria, inputBox);
+    }
+}
+
+function searchCriteria_onClick(criteria, inputBox) {
+    inputBox.onclick = function () {
+        alert("click " + inputBox.value + ':  ' + inputBox.checked);
+        if (inputBox.checked) {
+            var index = criteria.tags.findIndex(function (v) { return v == inputBox.value; });
+
+            if (index == -1) {
+                criteria.tags.push(inputBox.value);
+            }
+        }
+        else {
+            var index = criteria.tags.findIndex(function (v) { return v == inputBox.value; });
+
+            if (index >= 0) {
+                criteria.tags.splice(index, 1);
+            }
+        }
+
+        searchCriteria_onChangeCriteria(criteria);
+    }
+}
+
+function searchCriteria_onChangeCriteria(criteria) {
+    alert(JSON.stringify(criteria));
+}
--- CognitiveHackSol/PhotoBrowser/wwwroot/scripts/pages/search.js
+++ CognitiveHackSol/PhotoBrowser/wwwroot/scripts/pages/search.js
@@ -1,6 +1,7 @@
-﻿function bootstrapSearch() {
+﻿function bootstrapSearch(tagCount) {
     var imageResult = document.getElementById('imageResult');
     var imageResultTemplate = document.getElementById('imageResultTemplate');
 
+    searchCriteria_bindCriteria(tagCount);
     search_searchNoCriteria(imageResult, imageResultTemplate);
 }
